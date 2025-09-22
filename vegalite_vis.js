afa2a91a1b266fc6055bc3195b3a2635f6afeae1
RandalWallace <126451511+RandalWallace@users.noreply.github.com>
2025-09-22 19:53:37 +1000

    Update vegalite_vis.js

--- vegalite_vis.js
+++ vegalite_vis.js
@@ -1,4 +1,4 @@
-vegaEmbed('#map', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/chlropleth_map.vg.json', {actions: true})
+vegaEmbed('#map', 'https://github.com/RandalWallace/FIT3179_Assignment2/blob/d1f48cfe99f37b86ca4d9852f0b8e3a8e9241ff1/chlropleth_map.vg.json', {actions: true})
     .then(result => {
     console.log("Vega-Lite chart loaded from external file!");
     }).catch(console.error);
