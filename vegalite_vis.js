76a043c0dc2d285c6d238743ad82deaca8186c23
RandalWallace <126451511+RandalWallace@users.noreply.github.com>
2025-09-27 19:18:01 +1000

    Update vegalite_vis.js

--- vegalite_vis.js
+++ vegalite_vis.js
@@ -1,6 +1,6 @@
 vegaEmbed('#map', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/chlropleth_map.vg.json', {actions: true})
     .then(result => {
-    console.log("Vega-Lite chart loaded from external file!");
+        console.log("Map loaded successfully!");
     }).catch(console.error);
 
 vegaEmbed('#parallel_coordinate', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/parallel_coordinate.vg.json', {
@@ -11,3 +11,6 @@ vegaEmbed('#parallel_coordinate', 'https://raw.githubusercontent.com/RandalWalla
         editor: true
     }
 })
+.then(result => {
+    console.log("Parallel coordinate chart loaded successfully!");
+}).catch(console.error);
