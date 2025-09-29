f02a21002b62e3ea8d8cb43adcb31ce382ba5b15
RandalWallace <126451511+RandalWallace@users.noreply.github.com>
2025-09-29 15:17:10 +1000

    Update vegalite_vis.js

--- vegalite_vis.js
+++ vegalite_vis.js
@@ -1,16 +1,34 @@
 vegaEmbed('#map', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/chlropleth_map.vg.json', {actions: true})
     .then(result => {
-        console.log("Map loaded successfully!");
+    console.log("Vega-Lite chart loaded from external file!");
     }).catch(console.error);
 
-vegaEmbed('#parallel_coordinate', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/parallel_coordinate.vg.json', {
-    actions: {
-        export: true,
-        source: false,
-        compiled: false,
-        editor: true
-    }
-})
-.then(result => {
-    console.log("Parallel coordinate chart loaded successfully!");
-}).catch(console.error);
+vegaEmbed('#parallel_coordinate', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/parallel_coordinate.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
+
+vegaEmbed('#bar_chart', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/bar_chart.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
+
+vegaEmbed('#bubble_chart1', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/bubble_plot1.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
+
+vegaEmbed('#bubble_chart2', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/bubble_plot2.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
+
+vegaEmbed('#radar_chart', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/radar_chart.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
+
+vegaEmbed('#bump_chart', 'https://raw.githubusercontent.com/RandalWallace/FIT3179_Assignment2/refs/heads/main/bump_chart.vg.json', {actions: true})
+    .then(result => {
+    console.log("Vega-Lite chart loaded from external file!");
+    }).catch(console.error);
